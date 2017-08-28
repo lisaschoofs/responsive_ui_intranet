81e0f42776c7ed2f1984cfe74f0b21ccb1deccae
Lisa Schoofs <lisaschoofs@gmail.com>
2017-08-28 16:33:50 -0500

    container div fixes, adds dropdown triangles

--- _src/js/site.js
+++ _src/js/site.js
@@ -5,34 +5,7 @@ $(document).ready(function(){
 
 function eventListeners() {
   $("#sidemenu").on('click', function(){
-    console.log('you clicked sidemenu!');
-    $( "#sidenav" ).toggle( "slow" );
+    console.log('you clicked side menu!');
+    $( ".sidenav" ).toggle( "slow" );
   });
-
 }
-
-//
-//
-// $("#listingForm").on("submit", function(event){
-// event.preventDefault();
-// console.log('submit button clicked');
-// var type = $("#propertyType").val();
-// var city = $("#cityInput").val();
-// var sqft = $("#sqft").val();
-// var price = $("#price").val();
-// console.log(type, city, sqft, price);
-//
-//   $.ajax({
-//     type: "POST",
-//     url: "/listings",
-//     data: {type: type, city: city, sqft: sqft, price: price},
-//     success: function(response) {
-//       getListings(response);
-//       console.log(response);
-//     }//end success
-//   }); //end ajax
-//   //resets form on submit
-//   $("#listingForm").trigger('reset');
-//   $( "#listingForm" ).toggle( "slow" );
-// }); //end listener
-// }//end form submit
