50e0644e4d9ebc674fc096fae7ddf1be1070f340
Lisa Schoofs <lisaschoofs@gmail.com>
2017-09-07 10:59:40 -0500

    adds photos, progress on upcoming events, adds skyline img

--- _src/js/site.js
+++ _src/js/site.js
@@ -4,6 +4,10 @@ $(document).ready(function(){
 
 //Listeners for sideNav toggling
 function eventListeners() {
+  $("#search").on('click', function(){
+    $( ".searchBar").toggle();
+  })
+
   $("#sidemenu").on('click', function(){
     $( ".sidenav" ).toggle( "slow" );
   });
@@ -12,7 +16,28 @@ function eventListeners() {
     $( ".sidenav" ).toggle( "slow" );
   });
 
-  // $(".recentlySigned").('click', function(){
-  //   $();
-  // })
+  $(".marketNews").on('click', function(){
+    console.log('clicked header!');
+    $( ".marketNewsList" ).toggle( "fast" );
+  });
+
+  $(".announcements").on('click', function(){
+    console.log('clicked header!');
+    $( ".announcementsList" ).toggle( "fast" );
+  });
+
+  $(".recentlySigned").on('click', function(){
+    console.log('clicked recentlySigned header!');
+    $( ".recentlySignedList" ).toggle( "fast" );
+  });
+
+  $(".upcomingEvents").on('click', function(){
+    console.log('clicked upcomingEvents header!');
+    $( ".upcomingEventsList" ).toggle( "fast" );
+  });
+
+  $(".shortcuts").on('click', function() {
+    console.log('clicked shortcuts! Booyah!');
+    // $(".headerTiles").toggle();
+  })
 }
