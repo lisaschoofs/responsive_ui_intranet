68aa6f712dec1d9a736aad9883d39f5f1de41ed0
Lisa Schoofs <lisaschoofs@gmail.com>
2017-09-08 11:41:14 -0500

    adds caret animations

--- _src/js/site.js
+++ _src/js/site.js
@@ -1,55 +1,70 @@
 $(document).ready(function(){
   mainPageListeners();
   sidenavListeners();
+  // caretRotate();
 });
 
-//Listeners for sideNav toggling
 function mainPageListeners() {
   $("#search").on('click', function(){
     $( ".searchBar").toggle();
+    $(this).children().last().toggleClass("down");
   })
 
   $("#sidemenu").on('click', function(){
     $( ".sidenav" ).toggle( "slow" );
+    $(this).children().last().toggleClass("down");
   });
 
   $(".dropdownHeader").on('click', function(){
     $( ".sidenav" ).toggle( "slow" );
+    $(this).children().last().toggleClass("down");
   });
 
   $(".marketNews").on('click', function(){
     console.log('clicked header!');
     $( ".marketNewsList" ).toggle( "fast" );
+    $(this).children().last().toggleClass("down");
   });
 
   $(".announcements").on('click', function(){
     console.log('clicked header!');
     $( ".announcementsList" ).toggle( "fast" );
+    $(this).children().last().toggleClass("down");
   });
 
   $(".recentlySigned").on('click', function(){
     console.log('clicked recentlySigned header!');
     $( ".recentlySignedList" ).toggle( "fast" );
+    $(this).children().last().toggleClass("down");
   });
 
   $(".upcomingEvents").on('click', function(){
     console.log('clicked upcomingEvents header!');
     $( ".upcomingEventsList" ).toggle( "fast" );
+    $(this).children().last().toggleClass("down");
   });
 }
 
 function sidenavListeners() {
   $(".sitenav").on('click', function() {
-    console.log('clicked sitenav! Huzzah!');
     $(".sitenavList").toggle('fast');
+    $(this).children().last().toggleClass("down");
   });
 
   $(".myLinks").on('click', function() {
     console.log('clicked myLinks! Yaaaaas!');
+    $(this).children().last().toggleClass("down");
   });
 
   $(".shortcuts").on('click', function() {
-    console.log('clicked shortcuts! Awwww yeah.');
     $(".icons").toggle('fast');
+    $(this).children().last().toggleClass("down");
   });
 };
+
+// function caretRotate() {
+//   $(".triangleRotate").click(function(){
+//     $(this).toggleClass("down");
+//     console.log('MOVE! THAT! CARET!');
+//   });
+// }
